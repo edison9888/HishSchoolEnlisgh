fbab4590b98af2c725f5d72fd88fd9521962b336
ibokan <ibokan@2-19.local>
2013-01-05 16:29:21 +0800

    Initial Commit

---
+++ my/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  my
+//
+//  Created by ibokan on 13-1-5.
+//  Copyright (c) 2013年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
